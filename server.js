392c55b8da05bf10bd0e14492c18413ab49c2418
hidecard <hidecard1500@gmail.com>
2025-06-14 15:08:01 +0630

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'components')); // Custom views folder
+
+app.get('/contact', (req, res) => {
+    const name = "Contact Us";
+    res.render('nav', {name : name})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
