db2ac0f650797c1c9b8cea14baea673c47e8884b
hidecard <hidecard1500@gmail.com>
2025-06-14 16:04:00 +0630

    day 6

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'components')); // Custom views folder
 
 app.get('/', (req, res) => {
-    const item = ["apple" , "orange" , "Kiwi"]
-    res.render('nav', { item : item})
+    const name = "Mg Mg";
+    res.render('nav', { name : name})
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
